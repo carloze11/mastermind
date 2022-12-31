0d17aa6d4c5b271304a43c4fc1c8612ffff22103
Carlos Estrada <cestrada1109@gmail.com>
2022-12-30 21:05:08 -0600

    Add login static method and some validation

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -27,6 +27,17 @@ const userSchema = Schema(
 
 // static signup method for user
 userSchema.statics.signup = async function (email, password) {
+    //validation
+    if (!email || !password) {
+        throw Error("All fields must be filled");
+    }
+    if (!validator.isEmail(email)) {
+        throw Error("Email is not valid");
+    }
+    if (!validator.isStrongPassword(password)) {
+        throw Error("Password not strong enough");
+    }
+
     const exists = await this.findOne({ email });
 
     if (exists) {
@@ -40,6 +51,28 @@ userSchema.statics.signup = async function (email, password) {
     //store password in db
     const user = await this.create({ email, password: hash });
 
+    return user;
+};
+
+// static login method
+userSchema.statics.login = async function (email, password) {
+    // validation
+    if (!email || !password) {
+        throw Error("All fields must be filled");
+    }
+
+    const user = await this.findOne({ email });
+
+    if (!user) {
+        throw Error("Incorrect email");
+    }
+
+    // compare entered password to bcrypt
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+        throw Error("Incorrect password");
+    }
     return user;
 };
 
