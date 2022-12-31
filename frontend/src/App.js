44774f44b1a470b4f76c4698599350281e824b9d
Carlos Estrada <cestrada1109@gmail.com>
2022-12-30 21:36:11 -0600

    Add sign up hook and page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,17 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 // pages
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
                 </Routes>
             </BrowserRouter>
         </div>
---
+++ frontend/src/hooks/useSignup.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export const useSignUp = () => {
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(null);
+
+    const signup = async (email, password) => {
+        setIsLoading(true);
+        setError(null);
+
+        const response = await fetch("/user/signup", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email, password }),
+        });
+
+        const json = await response.json();
+
+        if (!response.ok) {
+            setIsLoading(false);
+            setError(json.error);
+        }
+        if (response.ok) {
+            setIsLoading(false);
+        }
+    };
+    return { signup, isLoading, error };
+};
