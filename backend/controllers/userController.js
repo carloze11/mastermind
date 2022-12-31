fd7627c132a7a5dda102295d54554095bd949759
Carlos Estrada <cestrada1109@gmail.com>
2022-12-30 20:51:13 -0600

    add signup functionality to user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,23 @@
+const User = require("../models/userModel");
+
 // login user
 const loginUser = (req, res) => {
     res.send("This is the login page.");
 };
 
-const signupUser = (req, res) => {
-    res.send("this is sign up page");
+// signup user
+const signupUser = async (req, res) => {
+    // get email and password from params
+    const { email, password } = req.body;
+
+    try {
+        // create user using model static method
+        const user = await User.signup(email, password);
+
+        res.status(200).json({ email, token });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 };
 
 module.exports = { loginUser, signupUser };
