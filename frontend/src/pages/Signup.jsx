55d50149fc6d95db29cf3a5a893784640b264a88
Carlos Estrada <cestrada1109@gmail.com>
2023-01-08 22:54:16 -0600

    Added password confirmation on signup

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -4,11 +4,12 @@ import useSignup from "../hooks/useSignup";
 const Signup = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
     const { signup, error, isLoading } = useSignup();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        await signup(email, password);
+        await signup(email, password, confirmPassword);
     };
 
     return (
@@ -27,6 +28,13 @@ const Signup = () => {
                 type="password"
                 onChange={(e) => setPassword(e.target.value)}
                 value={password}
+                className="input"
+            />
+            <label>Confirm Password:</label>
+            <input
+                type="password"
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                value={confirmPassword}
                 className="input"
             />
             {/* disabled btn to prevent resubmit */}
